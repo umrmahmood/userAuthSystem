dedfe2e2e0e83998fecf29087f7074e80a694f23
EugeneeT <vina.botanic@gmail.com>
2024-04-09 10:50:29 +0200

    ypdate for merge to main

--- testFront/userManager/src/components/Logout.jsx
+++ testFront/userManager/src/components/Logout.jsx
@@ -5,7 +5,8 @@ const Logout = () => {
 
     return (
         <div>
-
+            <p>Thank you, you have been successfully logged out.</p>
+            <p>You will be redirected in a few sec..</p>
         </div>
 
     )
