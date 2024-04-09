5a4e2c42fc2d10ab21f512b3235ca4411e353734
EugeneeT <vina.botanic@gmail.com>
2024-04-09 14:49:50 +0200

    update

--- testFront/userManager/src/App.jsx
+++ testFront/userManager/src/App.jsx
@@ -4,7 +4,7 @@ import Signup from './components/Signup.jsx';
 import AdminPage from './components/Admin.jsx';
 import Login from './components/Login.jsx';
 import Logout from './components/Logout.jsx';
-//import Profile from './components/Profile.jsx';
+import { Profile } from './components/Profile.jsx';
 import { useState, useEffect } from 'react';
 
 
@@ -51,7 +51,6 @@ function App() {
         <Route path="/create-user" element={<Signup navigate={navigate} />}></Route>
         <Route path="/admin-area" element={<AdminPage />}></Route>
         <Route path="/logged-out" element={<Logout />}></Route>
-        {/* if possible make a redirect to Homepage after 5 sec.  */}
       </Routes>
 
     </div>
