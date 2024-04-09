2fe63bba3c4d73d46b1a883285be3039326c6743
NadiNadiya <135104478+NadiNadiya@users.noreply.github.com>
2024-04-09 12:38:28 +0200

    log out endpoint added, added route to logout

--- logFunction/logout.js
+++ logFunction/logout.js
@@ -2,14 +2,26 @@ import express from "express";
 // logout endpoint
 export const logoutUser = async (req, res, next) => {
     try{
-        res.status(200).json({success:true, message:'Logout successfully'})
-    } catch(e){
-        next(e);
-    }
-};
-
-// clearing token from localStorage
-
-function clearToken(){
+        // Clearing token from localStorage
+    localStorage.removeItem('auth');//name for jwt token
+    console.log('Token cleared');
     
-}
+        //Handling logout through the server
+   const response = await  fetch('/logout', {
+    method:'POST',
+    headers: {
+    'Authorization': `Bearer ${localStorage.getItem('auth')}`
+    }})
+
+        if(response.ok){
+            res.status(200).json({success:true, message:'User logged out'})
+            console.log('Logged out');
+        } else {
+            res.status(404).json({success:false, message:'Failed to log out'});
+            console.error('Logout Failed');
+        }
+    } catch(err){
+        res.status(500).json({success:false, message:'Error during logout'});
+        console.error('Error occured :', err)
+    } }
+    
